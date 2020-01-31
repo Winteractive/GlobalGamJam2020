4dd80e8dadf0ac404a0188c33ef8071c300189b0
Max <max@winteractive.se>
2020-01-31 22:42:17 +0100

    First batch of animations!
    Idle, fallback and ChargeUp

---
+++ GlobalGamJam2020/Assets/Scripts/Editor/SpriteImport.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+class SpriteImport : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter; // create a TextureImporter object, this gives us tools to let us tweak the import settings on the current asset
+
+        if(textureImporter.textureType == TextureImporterType.Sprite)
+        {
+            textureImporter.spriteImportMode = SpriteImportMode.Single; // .. lets assume that each sprite is singular. if its a sprite sheet you have to set that up yourself the normal way
+            textureImporter.filterMode = FilterMode.Point;
+            textureImporter.spritePixelsPerUnit = 40; // Turn off mip maps because they are *gross* 
+        }
+
+
+
+    }
+}
