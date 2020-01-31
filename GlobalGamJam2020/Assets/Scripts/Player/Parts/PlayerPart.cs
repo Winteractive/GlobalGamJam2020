0e084e01248507a0db4c1948803934a968004358
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2020-01-31 20:54:33 +0100

    Added comment sand removed PlayerNumber

--- GlobalGamJam2020/Assets/Scripts/Player/Parts/PlayerPart.cs
+++ GlobalGamJam2020/Assets/Scripts/Player/Parts/PlayerPart.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public abstract class PlayerPart : MonoBehaviour
 {
     protected int playerNumber;
+
+    //Runs from Player. And gets the playerNumber accosiaded with it
     public void Initialize(int playerNumber)
     {
         this.playerNumber = playerNumber;
         CustomStart();
     }
+    /// <summary>
+    /// Runs on Player's Start function. Use this instead of Unitys Start. 
+    /// </summary>
     public virtual void CustomStart()
     {
 
--- GlobalGamJam2020/Assets/Scripts/Player/Player.cs
+++ GlobalGamJam2020/Assets/Scripts/Player/Player.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[DisallowMultipleComponent]
 public class Player : MonoBehaviour
 {
     public int playerNumber;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Setup all player parts attached on this Gameobject
         List<PlayerPart> playerParts = new List<PlayerPart>();
         playerParts.AddRange(GetComponents<PlayerPart>());
         playerParts.AddRange(GetComponentsInChildren<PlayerPart>());
@@ -14,11 +17,5 @@ public class Player : MonoBehaviour
         {
             playerPart.Initialize(playerNumber);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
