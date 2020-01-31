482bdead12afc6900496ab9cd7037ee1d0a8243a
Max <max@winteractive.se>
2020-01-31 15:18:29 +0100

    Wwise Updated

--- GlobalGamJam2020/Assets/TestWwise.cs
+++ GlobalGamJam2020/Assets/TestWwise.cs
@@ -7,7 +7,7 @@ public class TestWwise : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AkSoundEngine.PostEvent("test_play", this.gameObject);
+        AkSoundEngine.PostEvent("test_play2", this.gameObject);
     }
 
     // Update is called once per frame
