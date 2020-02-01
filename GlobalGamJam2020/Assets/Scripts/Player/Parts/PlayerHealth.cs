c4f65af58bc90be66bcfeab342bfe74edb2a752d
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2020-02-01 15:12:21 +0100

    Fix errors

--- GlobalGamJam2020/Assets/Scripts/Player/Parts/PlayerHealth.cs
+++ GlobalGamJam2020/Assets/Scripts/Player/Parts/PlayerHealth.cs
@@ -14,11 +14,16 @@ public class PlayerHealth : PlayerPart, IMediatorListener
         GlobalMediator.AddListener(this);
     }
 
-    public struct BreakMessage
+    public struct BreakMessage 
     {
         public int playerNumber;
     }
+    public struct RepairMessage
+    {
+        public int playerNumber;
+        public int repairAmount;
 
+    }
     private void TakeDamage()
     {
         currenthealth--;
@@ -38,9 +43,13 @@ public class PlayerHealth : PlayerPart, IMediatorListener
         }
     }
 
-    private void GetRepaired()
+    private void GetRepaired(int amount)
     {
-        currenthealth = maxHealth;
+        Debug.Log("Getting Repaired");
+        currenthealth += amount;
+
+        if (currenthealth > maxHealth)
+            currenthealth = maxHealth;
         //GlobalMediator.SendMessage(GameEvents.PLAYER_REPAIRED, new BreakMessage
         //{
         //    playerNumber = playerNumber
@@ -51,12 +60,20 @@ public class PlayerHealth : PlayerPart, IMediatorListener
     {
         if (events.HasFlag(GameEvents.PLAYER_TAKE_DAMAGE))
         {
+            
             //compare event player number to this player number
             TakeDamage();
         }
         if (events.HasFlag(GameEvents.PLAYER_REPAIRED))
         {
             //compare event player number to this player number
+            if(data is RepairMessage repairMessage)
+            {
+                if(playerNumber == repairMessage.playerNumber)
+                {
+                    GetRepaired(repairMessage.repairAmount);
+                }
+            }
         }
         if (events.HasFlag(GameEvents.PLAYER_CHARGE_RELEASED))
         {
--- GlobalGamJam2020/Assets/Scripts/Player/Parts/TagCheck.cs
+++ GlobalGamJam2020/Assets/Scripts/Player/Parts/TagCheck.cs
@@ -6,13 +6,13 @@ public class TagCheck : PlayerPart
 {
     public string tagToTriggerOn;
     public GameEvents triggerEventsToTrigger;
-    List<GameObject> objectsInside;
+    List<GameObject> objectsInside = new List<GameObject>();
 
     public struct TagCheckMessage
     {
         public int playerNumber;
         public bool triggerInside;
-        public List<GameObject> objectsInside;
+        public GameObject objectsInside;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,7 +26,7 @@ public class TagCheck : PlayerPart
             {
                 playerNumber = playerNumber,
                 triggerInside = true,
-                objectsInside = objectsInside
+                objectsInside = collision.gameObject
             });
         }
     }
@@ -35,12 +35,14 @@ public class TagCheck : PlayerPart
         if (collision.CompareTag(tagToTriggerOn) && collision.transform != transform.parent)
         {
             objectsInside.Remove(collision.gameObject);
+
             if (objectsInside.Count == 0)
             {
                 GlobalMediator.SendMessage(triggerEventsToTrigger, new TagCheckMessage
                 {
                     playerNumber = playerNumber,
-                    triggerInside = false
+                    triggerInside = false,
+                    objectsInside = collision.gameObject
                 });
             }
             
