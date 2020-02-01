2365233523b00e75f9408b89947f8e09e3dc9c92
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2020-02-01 21:30:57 +0100

    Fixing RepairZone bug, healing to fast

--- GlobalGamJam2020/Assets/Scripts/Player/Parts/PlayerPart.cs
+++ GlobalGamJam2020/Assets/Scripts/Player/Parts/PlayerPart.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class PlayerPart : MonoBehaviour
 {
-    public int playerNumber;
+    internal int playerNumber;
 
     //Runs from Player. And gets the playerNumber accosiaded with it
     public virtual void Initialize(int playerNumber)
