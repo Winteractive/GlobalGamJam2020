53a9efd8d153506ab6be7842ff6f48f06fc66d66
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2020-02-01 15:04:37 +0100

    Added RepairZone on player

--- GlobalGamJam2020/Assets/RepairZone.cs
+++ GlobalGamJam2020/Assets/RepairZone.cs
@@ -2,19 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RepairZone : PlayerPart,IMediatorListener
+public class RepairZone : PlayerPart, IMediatorListener
 {
+    List<int> playersInsideTrigger = new List<int>();
+    public int repairAmountPerUpdate;
+    public float timeBeforeRepair = 0.5f;
+    float timer;
+
+
+    public override void Initialize(int playerNumber)
+    {
+        base.Initialize(playerNumber);
+        GlobalMediator.AddListener(this);
+    }
+    private void Update()
+    {
+        
+
+        if (timer > timeBeforeRepair)
+        {
+            timer = 0;
+            foreach (var playerToRepair in playersInsideTrigger)
+            {
+                GlobalMediator.SendMessage(GameEvents.PLAYER_REPAIRED, new PlayerHealth.RepairMessage { playerNumber = playerToRepair, repairAmount = repairAmountPerUpdate });
+            }
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (playersInsideTrigger.Count == 0)
+            timer = 0;
+    }
+
     public void OnMediatorMessageReceived(GameEvents events, object data)
     {
-       if(events.HasFlag(GameEvents.PLAYER_REPAIR))
+        if(events.HasFlag(GameEvents.PLAYER_REPAIRED))
         {
             if(data is TagCheck.TagCheckMessage tagMessage)
             {
                 if(tagMessage.playerNumber == playerNumber)
                 {
-                    foreach (var item in tagMessage.objectsInside)
+                    if(tagMessage.triggerInside)
                     {
+                        playersInsideTrigger.Add(tagMessage.objectsInside.GetComponent<Player>().playerNumber);
 
+                    }
+                    else
+                    {
+                        playersInsideTrigger.Remove(tagMessage.objectsInside.GetComponent<Player>().playerNumber);
                     }
                 }
             }
